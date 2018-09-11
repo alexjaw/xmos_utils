b7f0a756ab19c77901b04087584367d6b24cfb0c
Alexander Jaworowski <alexander@jaworowski.se>
2018-09-11 13:59:19 +0200

    Improved test - make sure that transformation is doing something

--- src/tests/test_msb_to_lsb.py
+++ src/tests/test_msb_to_lsb.py
@@ -1,8 +1,12 @@
 import xmos_image_utils
 
 
-def test_msb_to_lsb(data=None):
+def test_msb_to_lsb():
     # Transforming msb to lsb twice must return the same stuff
     expected = b'\x80\x01'
-    actual = xmos_image_utils.msb_to_lsb(xmos_image_utils.msb_to_lsb(expected))
+    actual = xmos_image_utils.msb_to_lsb(expected)
+    # Make sure something happened
+    assert expected != actual
+    actual = xmos_image_utils.msb_to_lsb(actual)
+    # Make sure we got back same stuff after 2:nd transformation
     assert expected == actual
